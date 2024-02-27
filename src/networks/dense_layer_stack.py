38a54a09236103297e715e2acc7fe7157b17a1f0
Dominik Baron <dominik.b4ron@gmail.com>
2024-02-28 00:59:20 +0100

    Continue implementation of dense layer stacks

--- src/networks/dense_layer_stack.py
+++ src/networks/dense_layer_stack.py
@@ -1,45 +1,78 @@
-from typing import Callable, Literal
+import abc
+from typing import Literal
 
 import numpy as np
 import torch
 from torch import nn
 
-from src.networks.nn_base import NNBase
-from src.networks.skip_connection import AdditiveSkipConnection, ConcatSkipConnection
 from src.networks.layer_stack import LayerStack
-from src.networks.weighing import WeighingTypes, WeighingTrainableChoices
+from src.networks.weighing import WeighingTrainableChoices
 
+class DenseLayerStack(LayerStack, abc.ABC):
 
-class AdditiveDenseLayerStack(LayerStack):
+    ConnectionsType = tuple[tuple[int, int]] | list[list[int]] | np.ndarray \
+                      | Literal['dense', 'sequential']  # TODO Module
 
-    def __init__(
-            self,
-            layer_provider: Callable[[int, int, bool], nn.Module],
-            num_layers: int = None,
-            num_features: int = None,
-
-            connections: tuple[tuple[int]] | list[list[int]] | np.ndarray |  # TODO: module
-                        Literal['dense', 'sequential'] = 'dense',
-
-            weights_trainable: WeighingTrainableChoices = False,
-            initial_direct_connection_weight: WeighingTypes = 1.0,
-            initial_skip_connection_weight: WeighingTypes = 1.0,
-
-            # dropout_p: float = 0.0,
-            # normalization_provider: NNBase.Provider = None,
-    ):
-        super().__init__(layer_provider, num_layers=num_layers, num_features=num_features)
-
+    @staticmethod
+    def connections_to_np(
+            connections: ConnectionsType,
+            num_layers: int,
+    ) -> np.ndarray:
         if isinstance(connections, str):
             if connections in ['dense', 'sequential']:
                 # noinspection PyTypeChecker
-                connections = self.create_connections_by_name(connections, num_layers)
+                connections = DenseLayerStack.create_connections_by_name(connections, num_layers)
             else:
                 raise ValueError('Unknown connection type name')
         else:
             connections = np.array(connections)
 
         connections = (connections % (num_layers + 1))
+        return connections.astype(int)
+
+    @staticmethod
+    def create_connections_by_name(
+            name: Literal['dense', 'sequential'],
+            num_layers: int,
+    ) -> np.ndarray:
+        if name == 'dense':
+            connections = np.array([
+                [i, j]
+                for i in range(0, num_layers + 1)
+                for j in range(i, num_layers + 1)
+            ])
+        elif name == 'sequential':
+            connections = np.array([
+                [i, i]
+                for i in range(0, num_layers + 1)
+            ])
+        else:
+            raise ValueError
+
+        connections = connections
+        return connections
+
+
+class AdditiveDenseLayerStack(DenseLayerStack):
+
+    def __init__(
+            self,
+            layer_provider: LayerStack.LayerProvider,
+            num_layers: int,
+            num_features: int = None,
+
+            connections: DenseLayerStack.ConnectionsType = 'dense',
+
+            weights_trainable: WeighingTrainableChoices = False,
+            initial_direct_connection_weight: float = 1.0,
+            initial_skip_connection_weight: float = 1.0,
+
+            # dropout_p: float = 0.0,
+            # normalization_provider: NNBase.Provider = None,
+    ):
+        super().__init__(layer_provider, num_layers=num_layers, num_features=num_features)
+
+        connections = self.connections_to_np(connections, num_layers)
 
         assert (connections >= 0).all()
         assert (connections <= num_layers).all()
@@ -56,53 +89,88 @@ class AdditiveDenseLayerStack(LayerStack):
                                            if from_idx == to_idx
                                            else initial_skip_connection_weight)
 
-        self.num_layers = num_layers
-        self.num_features = num_features
-        self.connections = connections
-
+        self.connections = nn.Parameter(torch.FloatTensor(connections), requires_grad=False)
         self.mask = nn.Parameter(mask, requires_grad=False)
         self.weight = nn.Parameter(weight, requires_grad=weights_trainable)
 
+    def forward(self, x: torch.Tensor, return_dense=False, **kwargs):
 
-    def forward(self, x: torch.Tensor, return_connection_node_values=False, **kwargs):
-        connection_node_values = torch.zeros_like(x.float()) \
+        dense_tensor = torch.zeros_like(x.float()) \
                 .unsqueeze(-1).repeat_interleave(self.num_layers + 1, dim=-1)
-        connection_node_values[..., 0] = x
+        dense_tensor[..., 0] = x
 
         for i, layer in enumerate(self.layers):
-
-            layer_input = (connection_node_values * self.mask[i] * self.weight[i]).sum(dim=-1)
+            layer_input = (dense_tensor * self.mask[i] * self.weight[i]).sum(dim=-1)
             layer_output = layer(layer_input, **kwargs)
-            connection_node_values[..., i + 1] = layer_output
+            dense_tensor[..., i + 1] = layer_output
 
-        if return_connection_node_values:
-            return connection_node_values
+        out = (dense_tensor * self.mask[-1] * self.weight[-1]).sum(dim=-1)
+
+        if return_dense:
+            return out, dense_tensor
         else:
-            return connection_node_values[..., -1]
+            return out
+
+
+class ConcatDenseLayerStack(DenseLayerStack):
 
     @staticmethod
-    def create_connections_by_name(
-            name: Literal['dense', 'sequential'],
-            num_layers: int,
+    def compute_layers_cum_in_out_sizes(
+            layers_in_out_sizes: list[tuple[int, int]] = None,
+            layers_sizes: list[int] = None,
+            num_layers: int = None,
+            num_features: int = None,
+            connections: DenseLayerStack.ConnectionsType = 'dense',
     ):
-        if name == 'dense':
-            connections = np.array([
-                    [i, j]
-                    for i in range(0, num_layers + 1)
-                    for j in range(i, num_layers + 1)
-                ])
-        elif name == 'sequential':
-            connections = np.array([
-                    [i, i]
-                    for i in range(0, num_layers + 1)
-                ])
-        else:
-            raise ValueError
+        layers_in_out_sizes = DenseLayerStack.compute_layers_in_out_sizes(
+            layers_in_out_sizes, layers_sizes, num_layers, num_features)
 
-        connections: np.ndarray = (connections % (num_layers + 1))
-        return connections
+        num_layers = len(layers_in_out_sizes)
+        in_sizes = np.array([in_size for in_size, out_size in layers_in_out_sizes])
+        connections = DenseLayerStack.connections_to_np(connections, num_layers)
+
+        layers_cum_in_out_sizes: list[tuple[int, int]] = []
+        for i in range(num_layers):
+            in_size_sum = in_sizes[connections[connections[:, 1] == i][:, 0]].sum()
+            layers_cum_in_out_sizes.append((in_size_sum, layers_in_out_sizes[i][-1]))
+
+        return layers_cum_in_out_sizes
+
+    def __init__(
+            self,
+            layer_provider: LayerStack.LayerProvider,
+            num_layers: int = None,
+            num_features: int = None,
+            layers_sizes: list[int] = None,
+            connections: DenseLayerStack.ConnectionsType = 'dense',
+    ):
+        super().__init__(
+            layer_provider,
+            layers_in_out_sizes=ConcatDenseLayerStack.compute_layers_cum_in_out_sizes(
+                None, layers_sizes=layers_sizes,
+                num_layers=num_layers, num_features=num_features,
+                connections=connections
+            )
+        )
+        self.connections = DenseLayerStack.connections_to_np(connections, self.num_layers)
 
 
+    def forward(self, x, *args, return_dense=False, return_dense_list=False, **kwargs):
+        layer_out = None
+        dense_tensor_list: list[torch.Tensor] = [x]
 
+        for i, layer in enumerate(self.layers):
+            sources = self.connections[self.connections[:, 1] == i][:, 0]
+            layer_dense_input = torch.cat([
+                dense_source
+                for j, dense_source in enumerate(dense_tensor_list)
+                if j in sources
+            ])
+            layer_out = layer(layer_dense_input)
+            dense_tensor_list.append(layer_out)
 
-
+        if return_dense:
+            return layer_out, torch.cat(dense_tensor_list)
+        if return_dense_list:
+            return layer_out, dense_tensor_list
+        return layer_out
--- src/networks/layer_stack.py
+++ src/networks/layer_stack.py
@@ -7,17 +7,17 @@ from src.networks.nn_base import NNBase
 
 class LayerStack(NNBase):
 
-    def __init__(
-            self,
-            layer_provider: Callable[[int, int, bool], nn.Module],
-            layers_in_out_sizes: list[tuple] = None,
+    LayerProvider = Callable[[int, bool, int, int], nn.Module]
+
+    @staticmethod
+    def compute_layers_in_out_sizes(
+            layers_in_out_sizes: list[tuple[int, int]] = None,
             layers_sizes: list[int] = None,
             num_layers: int = None,
             num_features: int = None
     ):
-        super().__init__()
-
-        assert num_layers is not None and num_features is not None or num_layers is None and num_features is None
+        # TODO
+        # assert num_layers is not None and num_features is not None or num_layers is None and num_features is None
 
         assert layers_in_out_sizes is None or layers_sizes is None
         assert layers_in_out_sizes is None or num_layers is None
@@ -30,28 +30,43 @@ class LayerStack(NNBase):
         if num_layers is not None:
             layers_in_out_sizes = [(num_features, num_features) for _ in range(num_layers)]
 
-
-        self.layers = self.create_layers(layer_provider, layers_in_out_sizes)
-        self.layer_stack = self.combine_layers(self.layers)
+        return layers_in_out_sizes
 
     @staticmethod
     def create_layers(
-            layer_provider: Callable[[int, int, bool], nn.Module],
-            layers_in_out_sizes: list[tuple] = None,
-    ) -> list[nn.Module]:
+            layer_provider: LayerProvider,
+            layers_in_out_sizes: list[tuple[int, int]] = None,
+    ) -> nn.ModuleList:
         layers: list[nn.Module] = []
 
         for i, (in_size, out_size) in enumerate(layers_in_out_sizes):
             is_final_layer = i == len(layers_in_out_sizes) - 1
-            layers.append(layer_provider(in_size, out_size, is_final_layer))
+            layers.append(layer_provider(i, is_final_layer, in_size, out_size))
 
-        return layers
+        return nn.ModuleList(layers)
 
-    @staticmethod
-    def combine_layers(layers: list[nn.Module]) -> nn.Module:
-        return nn.Sequential(*layers)
+    def __init__(
+            self,
+            layer_provider: LayerProvider,
+            layers_in_out_sizes: list[tuple[int, int]] = None,
+            layers_sizes: list[int] = None,
+            num_layers: int = None,
+            num_features: int = None,
+    ):
+        super().__init__()
+
+        self.layers_in_out_sizes = self.compute_layers_in_out_sizes(
+            layers_in_out_sizes,
+            layers_sizes,
+            num_layers,
+            num_features
+        )
+
+        self.num_layers = len(self.layers_in_out_sizes)
+
+        self.layers = nn.Sequential(*self.create_layers(layer_provider, self.layers_in_out_sizes))
 
     def forward(self, *args, **kwargs):
-        self.layer_stack(*args, **kwargs)
+        self.layers(*args, **kwargs)
 
 
